6fc50fa615ff3953f996fa8900e23e7c42732b90
MN-TG <mohammedmusammiln@gmail.com>
2025-05-24 11:39:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,4 +5,4 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    await message.reply_text("hello sir, how are you")
+    await message.reply_text("only for @MrMNTG")
