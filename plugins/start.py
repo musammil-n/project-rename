0c9119a06f6e32b4ef3888a4f7b8cca76365f359
MN-TG <mohammedmusammiln@gmail.com>
2025-05-24 18:32:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,4 +5,4 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    await message.reply_text("only for @MrMNTG")
+    await message.reply_text("Hey, This bot only works in its specific groups")
