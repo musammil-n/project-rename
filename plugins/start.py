b516140731fb3bad45877bb827ba6877418eb515
MN-TG <mohammedmusammiln@gmail.com>
2025-05-24 18:33:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,5 @@
-import random
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-  
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Hey, This bot only works in its specific groups")
+    await message.reply_text("Hey! This bot only works in its specific groups.")
